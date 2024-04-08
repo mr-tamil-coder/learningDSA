44526d59d379de4f0a42b9c56c158f229d5f0c2e
vinoth <mohanrajvinoth2004@gmail.com>
2024-04-08 19:21:52 +0530

    feat : count the digits

---
+++ Basic Concepts/countDigits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+//Count the number of Digits
+
+class countDigits{
+    //time complexity - O(n)
+    // and space complexity - O(1) 
+    public static int CountDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        int num=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the digits you want to count");
+        num = sc.nextInt();
+        int result=CountDigits(num);
+        System.out.println("The total digits is "+result);
+
+    }
+}
