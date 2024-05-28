1edc4aeac87919022de1430034d557b3594c8458
vinoth <mohanrajvinoth2004@gmail.com>
2024-05-28 18:41:26 +0530

    feat : gcd optimized soln added

--- Day2/gcd.java
+++ Day2/gcd.java
@@ -1,3 +1,26 @@
+import java.util.*;
 class gcd{
-    
+    public static int gcd(int a,int b){
+        while(a!=0 && b!=0){
+            if(a>b){
+                a=a%b;
+            }else{
+                b=b%a;
+            }
+        }
+        if(a==0){
+            return b;
+        }else{
+            return a;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of gcd a and b");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int result=gcd(a,b);
+        System.out.println("The gcd of"+a+" and "+b+" is "+result);
+    }
 }
+//Time complexity : O(log(min(a,b)))
