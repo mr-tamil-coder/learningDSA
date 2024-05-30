1500e14553e4a7180407c31339d92a98b786a5a8
vinoth <mohanrajvinoth2004@gmail.com>
2024-05-30 12:00:00 +0530

    feat : to find the primeFactors'

--- Day4/primeFactor.java
+++ Day4/primeFactor.java
@@ -1,6 +1,21 @@
+// 13.Find all the prime factors of a number
+import java.util.*;
 
 public class primeFactor {
+    public static void findPrimeFactors(int n){
+        // Time complexity - O(root(n)logn)
+        int i=2;
+        while(i<=Math.sqrt(n)){ //(or) Squaring on both sides  - i*i<=n
+            while(n%i==0){
+                System.out.println(i);
+                n=n/i;
+            }
+            i++;
+        }
+        if(n>1)
+         System.out.println(n);
+    }
     public static void main(String[] args) {
-        
+        findPrimeFactors(40);
     }
 }
