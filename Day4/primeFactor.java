099a36418e9d0ea49012ecfc24968fe4069675df
vinoth <mohanrajvinoth2004@gmail.com>
2024-05-30 12:00:00 +0530

    feat: print divisor of the number

---
+++ Day4/primeFactor.java
@@ -0,0 +1,6 @@
+
+public class primeFactor {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Day4/printDivisor.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class printDivisor {
+    public static void BruteForcePrintDivisor(int n){
+        // TIME - O(N)
+        for (int i = 1; i <=n; i++) {
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void optimizedPrintDivisor(int n){
+        int i=0;
+        for ( i = 1; i <= Math.sqrt(n); i++) {
+            if(n%i==0)
+                System.out.println(i);
+        }
+        i--;
+        for (; i >=1; i--) {
+            if(n%i==0 && i!=n/i)
+                System.out.println(n/i);
+
+        }
+    }
+    public static void main(String[] args) {
+//         BruteForcePrintDivisor(20);
+        optimizedPrintDivisor(20);
+
+    }
+}
