d1d0582a1dbdea02a70be3c140504c78561a0e1c
vinoth <mohanrajvinoth2004@gmail.com>
2024-06-03 10:00:00 +0530

    feat: kernighans Algorithm to find set bits

--- Day6/findRightMostBit.java
+++ Day6/findRightMostBit.java
@@ -5,7 +5,18 @@ public class findRightMostBit {
         int expo=(int)(Math.log10(result)/Math.log10(2));
         return expo+1;
     }
-      public static void main(String[] args) {
+    public static int kernighansAlgo(int n){
+        //19.count set(1)  bits in a number
+        // time complexity =O(logN)
+        int count=0;
+        while(n>0){
+            n=n&n-1;        
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
         System.out.println(toFindRightMostBit(8));
+        System.out.println(kernighansAlgo(42));
     }
 }
