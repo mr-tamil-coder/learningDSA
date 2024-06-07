ded58644d94f0ec2c59c7c0f99d20c221405263c
vinoth <mohanrajvinoth2004@gmail.com>
2024-06-07 12:00:00 +0530

    feat: to create a singleyLinkedList

---
+++ LinkedList 1/sll.java
@@ -0,0 +1,13 @@
+//to create a node of singly LinkedList
+class Node{
+    int data; 
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+ }
+ 
+public class sll {
+    
+}
